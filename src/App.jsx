1ab6a235d6cee217c1bc5c19da3788ab6eb9c6b3
ENDER ÇELİK <182804017@ogr.cbu.edu.tr>
2023-08-07 11:44:11 +0300

    form validation with formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,71 @@
 import './App.css';
 import logo from './assets/codeo-logo.png';
-function App() {
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    alert('lol');
-  };
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
+function App() {
   return (
     <>
       <div className='form-container'>
         <div className='codeo-img-container'>
           <img src={logo} alt='logo-codeo' />
         </div>
-        <form className='signIn-form' onSubmit={handleFormSubmit}>
-          <div className='client-id-container'>
-            <label htmlFor='client-id'>Client ID</label>
-            <input
-              required
-              placeholder='Enter Client ID'
-              type='text'
-              className='client-id'
-            />
-          </div>
 
-          <div className='client-secret-container'>
-            <label htmlFor='client-secret'>Client Secret</label>
+        <Formik
+          initialValues={{
+            clientId: '',
+            clientSecret: '',
+          }}
+          validationSchema={Yup.object({
+            clientId: Yup.string().required('Client ID can not be empty'),
+            clientSecret: Yup.string().required(
+              'Client Secret can not be empty'
+            ),
+          })}
+          onSubmit={(values) => console.log(values)}
+        >
+          {({ values, errors, handleChange, handleSubmit, dirty, touched }) => (
+            <form className='signIn-form' onSubmit={handleSubmit}>
+              <div className='client-id-container'>
+                <label htmlFor='clientId'>Client ID</label>
+                <input
+                  id='clientId'
+                  type='text'
+                  placeholder='Enter Client ID'
+                  className='client-id'
+                  value={values.clientId}
+                  onChange={handleChange}
+                />
+                {errors.clientId && touched.clientId && (
+                  <>
+                    <div className='input-error'>{errors.clientId}</div>
+                  </>
+                )}
+              </div>
 
-            <input
-              required
-              placeholder='Enter Client Secret'
-              type='text'
-              className='client-secret'
-            />
-          </div>
+              <div className='client-secret-container'>
+                <label htmlFor='clientSecret'>Client Secret</label>
+                <input
+                  id='clientSecret'
+                  placeholder='Enter Client Secret'
+                  type='text'
+                  className='client-secret'
+                  value={values.clientSecret}
+                  onChange={handleChange}
+                />
+                {errors.clientSecret && touched.clientSecret && (
+                  <>
+                    <div className='input-error'>{errors.clientSecret}</div>
+                  </>
+                )}
+              </div>
 
-          <button type='submit'>Sign In</button>
-        </form>
+              <button disabled={!dirty} type='submit'>
+                Sign In
+              </button>
+            </form>
+          )}
+        </Formik>
       </div>
     </>
   );
